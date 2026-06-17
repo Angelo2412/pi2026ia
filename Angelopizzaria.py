b0c311dfb29b4adc455197c7fa38afc3ad71ae24
Angelo2412 <Angelo@email>
2026-06-16 21:32:04 -0300

    alteração 2

--- Angelopizzaria.py
+++ Angelopizzaria.py
@@ -1,16 +1,16 @@
 pizzas = {
-"Calabresa":{"Pequeno":35.00,"Media":45.00,"Grande":55.00},
-"Mussarela":{"Pequeno":33.00,"Media":43.00,"Grande":53.00},
-"Frango com catupiry":{"Pequeno":38.00,"Media":48.00,"Grande":58.00},
-"Portuguesa":{"Pequeno":40.00,"Media":50.00,"Grande":60.00},
-"Bacon com milho":{"Pequeno":40.00,"Media":50.00,"Grande":60.00},
-"Quatro queijo":{"Pequeno":40.00,"Media":50.00,"Grande":60.00},
-"Coração":{"Pequeno":45.00,"Media":57.00,"Grande":70.00},
-"Strogonoff":{"Pequeno":55.00,"Media":60.00,"Grande":80.00}
+    "Calabresa": {"Pequeno": 35.00, "Media": 45.00, "Grande": 55.00},
+    "Mussarela": {"Pequeno": 33.00, "Media": 43.00, "Grande": 53.00},
+    "Frango com catupiry": {"Pequeno": 38.00, "Media": 48.00, "Grande": 58.00},
+    "Portuguesa": {"Pequeno": 40.00, "Media": 50.00, "Grande": 60.00},
+    "Bacon com milho": {"Pequeno": 40.00, "Media": 50.00, "Grande": 60.00},
+    "Quatro queijo": {"Pequeno": 40.00, "Media": 50.00, "Grande": 60.00},
+    "Coração": {"Pequeno": 45.00, "Media": 57.00, "Grande": 70.00},
+    "Strogonoff": {"Pequeno": 55.00, "Media": 60.00, "Grande": 80.00}
 }
 
 while True:
-    sabor = input("Digite o sabor da pizza (ou 'exit' para sair): ")
+    sabor = input("Digite o sabor da pizza (ou 'exit' para sair): ").title()
 
     if sabor.lower() == "exit":
         print("Programa encerrado.")
@@ -20,11 +20,13 @@ while True:
         print("Sabor não encontrado!")
         continue
 
-    tamanho = input("Digite o tamanho (P, M ou G): ").upper()
+    tamanho = input("Digite o tamanho (Pequeno, Media ou Grande): ").title()
 
     if tamanho not in pizzas[sabor]:
         print("Tamanho inválido!")
         continue
 
     valor = pizzas[sabor][tamanho]
-    print(f"Pedido: {sabor} ({tamanho}) - R$ {valor:.2f}")
+
+    print(f"\nPedido: {sabor} ({tamanho})")
+    print(f"Valor: R$ {valor:.2f}\n")
